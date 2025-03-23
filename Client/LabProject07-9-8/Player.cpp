585025d9dc9e8211ba0e6e0848bd087fc3ad49bf
xorud1210 <rhxo1210@naver.com>
2025-03-23 16:30:00 +0900

    플레이어 변경

--- Client/LabProject07-9-8/Player.cpp
+++ Client/LabProject07-9-8/Player.cpp
@@ -356,7 +356,7 @@ CTerrainPlayer::CTerrainPlayer(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandLi
 	m_pCamera = ChangeCamera(THIRD_PERSON_CAMERA, 0.0f);
 
 	CLoadedModelInfo *pAngrybotModel = CGameObject::LoadGeometryAndAnimationFromFile(pd3dDevice, pd3dCommandList,
-		pd3dGraphicsRootSignature, "Model/SK_Pig.bin", NULL);
+		pd3dGraphicsRootSignature, "Model/SK_Hu_M_FullBody_c.bin", NULL);
 	SetChild(pAngrybotModel->m_pModelRootObject, true);
 
 	int nAnimation{10};
