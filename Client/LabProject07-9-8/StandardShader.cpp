89b2977f97e26f91ff4434ab336272917610e990
xorud1210 <rhxo1210@naver.com>
2025-04-19 03:55:21 +0900

    셰이더 분할 및 수정

---
+++ Client/LabProject07-9-8/StandardShader.cpp
@@ -0,0 +1,29 @@
+#include "StandardShader.h"
+
+CStandardShader::CStandardShader() {}
+CStandardShader::~CStandardShader() {}
+
+D3D12_INPUT_LAYOUT_DESC CStandardShader::CreateInputLayout() {
+	// ... (기존 Standard Input Layout 코드) ...
+	UINT nInputElementDescs = 5;
+	D3D12_INPUT_ELEMENT_DESC* pd3dInputElementDescs = new D3D12_INPUT_ELEMENT_DESC[nInputElementDescs];
+	pd3dInputElementDescs[0] = { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+	pd3dInputElementDescs[1] = { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 1, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+	pd3dInputElementDescs[2] = { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 2, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+	pd3dInputElementDescs[3] = { "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 3, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+	pd3dInputElementDescs[4] = { "BITANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 4, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+	D3D12_INPUT_LAYOUT_DESC d3dInputLayoutDesc;
+	d3dInputLayoutDesc.pInputElementDescs = pd3dInputElementDescs;
+	d3dInputLayoutDesc.NumElements = nInputElementDescs;
+	return(d3dInputLayoutDesc);
+}
+
+D3D12_SHADER_BYTECODE CStandardShader::CreateVertexShader() {
+	// "StandardShader.hlsl" 파일의 "VSStandard" 함수를 컴파일하도록 수정
+	return(CShader::CompileShaderFromFile(L"StandardShaders.hlsl", "VSStandard", "vs_5_1", &m_pd3dVertexShaderBlob));
+}
+
+D3D12_SHADER_BYTECODE CStandardShader::CreatePixelShader() {
+	// "StandardShader.hlsl" 파일의 "PSStandard" 함수를 컴파일하도록 수정
+	return(CShader::CompileShaderFromFile(L"StandardShaders.hlsl", "PSStandard", "ps_5_1", &m_pd3dPixelShaderBlob));
+}
