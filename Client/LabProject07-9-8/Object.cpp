c4cc360468991b256b0777cd7fe5b93362bd1078
xorud1210 <rhxo1210@naver.com>
2025-07-23 16:56:07 +0900

    스플랫맵 적용

--- Client/LabProject07-9-8/Object.cpp
+++ Client/LabProject07-9-8/Object.cpp
@@ -1699,20 +1699,23 @@ CHeightMapTerrain::CHeightMapTerrain(ID3D12Device* pd3dDevice, ID3D12GraphicsCom
 
 	
 	std::shared_ptr<CTexture> pTerrainBaseTexture = pResourceManager->GetTexture(L"Terrain/DemoTerrain3.dds", pd3dCommandList);
+
 	std::shared_ptr<CTexture> pTerrainSplatMapTexture = pResourceManager->GetTexture(L"Terrain/Test.dds", pd3dCommandList);
 
 
 	// 풀 디테일 텍스쳐
-	std::shared_ptr<CTexture> pTerrainGrassTexture = pResourceManager->GetTexture(L"Terrain/grass.dds", pd3dCommandList);
+	std::shared_ptr<CTexture> pTerrainGrassTexture = pResourceManager->GetTexture(L"Terrain/TerrainGrass_basecolor.dds", pd3dCommandList);
 	// 흙 디테일 텍스쳐 
 	std::shared_ptr<CTexture> pTerrainDirtTexture = pResourceManager->GetTexture(L"Terrain/Detail_Texture_7.dds", pd3dCommandList);
 	// 돌 디테일 텍스쳐
 	std::shared_ptr<CTexture> pTerrainRockTexture = pResourceManager->GetTexture(L"Terrain/Detail_Texture_8.dds", pd3dCommandList);
 
 	if (pTerrainBaseTexture) pTerrainMaterial->AssignTexture(0, pTerrainBaseTexture, pd3dDevice);
-	if (pTerrainGrassTexture) pTerrainMaterial->AssignTexture(1, pTerrainGrassTexture, pd3dDevice);
+	if (pTerrainSplatMapTexture) pTerrainMaterial->AssignTexture(1, pTerrainSplatMapTexture, pd3dDevice);
 	if (pTerrainDirtTexture) pTerrainMaterial->AssignTexture(2, pTerrainDirtTexture, pd3dDevice);
-	if (pTerrainRockTexture) pTerrainMaterial->AssignTexture(3, pTerrainRockTexture, pd3dDevice);
+	if (pTerrainGrassTexture) pTerrainMaterial->AssignTexture(3, pTerrainGrassTexture, pd3dDevice);
+	if (pTerrainRockTexture) pTerrainMaterial->AssignTexture(4, pTerrainRockTexture, pd3dDevice);
+
 
 
 	
